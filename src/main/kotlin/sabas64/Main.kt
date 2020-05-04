ba40d8608586aef2f27f1aa4cf4a449fd9f90034
Sebastian Hungerecker <sepp2k@googlemail.com>
2020-05-04 23:17:44 +0200

    Extract code to apply rules

--- src/main/kotlin/sabas64/Main.kt
+++ src/main/kotlin/sabas64/Main.kt
@@ -1,20 +1,34 @@
 package sabas64
 
+import org.antlr.v4.runtime.CharStream
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
 import org.antlr.v4.runtime.tree.ParseTreeWalker
 import sabas64.rules.VariableNameChecker
 import kotlin.system.exitProcess
 
+object Main {
+    fun applyAllRules(input: CharStream, issueReporter: IssueReporter) {
+        val lexer = BasicLexer(input)
+        val parser = BasicParser(CommonTokenStream(lexer))
+        val program = parser.program()
+        val listeners = listOf<BasicListener>(
+            VariableNameChecker(issueReporter)
+        )
+        ParseTreeWalker.DEFAULT.walk(ProxyListener(listeners), program)
+    }
+}
+
 fun main(args: Array<String>) {
-    val lexer = BasicLexer(CharStreams.fromStream(System.`in`))
-    val parser = BasicParser(CommonTokenStream(lexer))
-    val program = parser.program()
     val issueReporter = IssueReporter()
-    val listeners = listOf<BasicListener>(
-        VariableNameChecker(issueReporter)
-    )
-    ParseTreeWalker.DEFAULT.walk(ProxyListener(listeners), program)
+    val input = if (args.isEmpty()) {
+        CharStreams.fromStream(System.`in`)
+    } else {
+        CharStreams.fromFileName(args[0])
+    }
+
+    Main.applyAllRules(input, issueReporter)
+
     if (issueReporter.issueCount > 0) {
         println("${issueReporter.issueCount} issues found")
     } else {
