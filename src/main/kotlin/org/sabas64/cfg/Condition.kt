403759322c1145b09c26a72aea6db49eb0bc95c0
Sebastian Hungerecker <sepp2k@googlemail.com>
2020-05-31 21:17:25 +0200

    Make Condition members accessible

--- src/main/kotlin/org/sabas64/cfg/Condition.kt
+++ src/main/kotlin/org/sabas64/cfg/Condition.kt
@@ -3,6 +3,6 @@ package org.sabas64.cfg
 import org.sabas64.BasicParser.*
 
 sealed class Condition {
-    class Expression(tree: ExpressionContext) : Condition()
-    class ForCondition(indexVariable: IdentifierContext, bound: ExpressionContext) : Condition()
+    class Expression(val tree: ExpressionContext) : Condition()
+    class ForCondition(val indexVariable: IdentifierContext, val bound: ExpressionContext) : Condition()
 }
